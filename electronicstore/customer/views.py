858a8f18447b3dd18dfff1b1e113c3d9342a10b0
vrindha <vrindhaaravind2410@gmail.com>
2021-10-11 18:34:13 +0530

    changeview

--- electronicstore/customer/views.py
+++ electronicstore/customer/views.py
@@ -1,46 +1,52 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView
 from customer import forms
 
+
 class RegistrationView(TemplateView):
-    form_class=forms.RegistrationForm
-    template_name="registration.html"
-    model=User
-    context={}
+    form_class = forms.RegistrationForm
+    template_name = "registration.html"
+    model = User
+    context = {}
 
-    def get(self,request,*args,**kwargs):
-        form=self.form_class()
-        self.context["form"]=form
-        return render(request,self.template_name,self.context)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        self.context["form"] = form
+        return render(request, self.template_name, self.context)
 
-    def post(self,request,*args,**kwargs):
-        form=self.form_class(request.POST)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
             return redirect("cust_signin")
 
 
 class SignInView(TemplateView):
-    template_name="login.html"
-    form_class=forms.LoginForm
-    context={}
+    template_name = "login.html"
+    form_class = forms.LoginForm
+    context = {}
 
-    def get(self,request,*args,**kwargs):
-        form=self.form_class()
-        self.context["form"]=form
-        return render(request,self.template_name,self.context)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        self.context["form"] = form
+        return render(request, self.template_name, self.context)
 
-    def post(self,request,*args,**kwargs):
-        form=self.form_class(request.POST)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
-            username=form.cleaned_data["username"]
-            password=form.cleaned_data["password"]
-            user=authenticate(request,username=username,password=password)
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, email=email, password=password)
             if user:
-                login(request,user)
+                login(request, user)
                 return redirect("customer_home")
             else:
-                self.context["form"]=form
-                return render(request,self.template_name,self.context)
+                self.context["form"] = form
+                return render(request, self.template_name, self.context)
+
+
+def signout(request):
+    logout(request)
+    return redirect("signin")
