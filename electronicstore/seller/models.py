1ca9698e184ca06a600f06d9da5288621a5e381f
Johny <joeljoshy143@gmail.com>
2021-10-11 17:26:33 +0530

    Seller Details Model

--- electronicstore/seller/models.py
+++ electronicstore/seller/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Seller_Details(models.Model):
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address = models.TextField()
+    bank_name = models.CharField(max_length=100)
+    account_number = models.IntegerField()
+    ifsc_code = models.CharField(max_length=20)
+
+
