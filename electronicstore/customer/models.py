5c5a329e60791cf5eb457ca9462efff6867ade21
Vrindha Aravind <86411764+VrindhaAravind@users.noreply.github.com>
2021-10-12 11:55:43 +0530

    Update models.py

--- electronicstore/customer/models.py
+++ electronicstore/customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Userdetails(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mobile_number = models.CharField(max_length=15,blank=False)
+    dob = models.DateField(blank=True,null=True)
+    image = models.ImageField(upload_to="images",blank=True)
+
+    def __str__(self):
+        return self.first_name
