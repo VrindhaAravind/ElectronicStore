aa73daa9520a3e07031c234169de150653cdd056
Vrindha Aravind <86411764+VrindhaAravind@users.noreply.github.com>
2021-10-12 11:56:49 +0530

    Update forms.py

--- electronicstore/customer/forms.py
+++ electronicstore/customer/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import ModelForm
+from .models import Userdetails
 
 
 class RegistrationForm(UserCreationForm):
@@ -21,3 +23,15 @@ class RegistrationForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"class": "form-control"}))
     password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+    class UpdateForm(ModelForm):
+    class Meta:
+        model = Userdetails
+        fields = "__all__"
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "mobile_number": forms.TextInput(attrs={"class": "form-control"}),
+            "dob": forms.DateInput(attrs={"class": "form-control", "placeholder": "yyyy-mm-dd"}),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+        }
