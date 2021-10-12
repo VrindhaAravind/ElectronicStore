3661e8d1b6fcbabeb043a2632634929e3a4cb710
Vrindha Aravind <86411764+VrindhaAravind@users.noreply.github.com>
2021-10-12 11:54:38 +0530

    Update urls.py

--- electronicstore/customer/urls.py
+++ electronicstore/customer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegistrationView,SignInView,HomePageView
+from .views import RegistrationView,SignInView,HomePageView,update_details
 
 urlpatterns=[
     path('register',RegistrationView.as_view(),name='register'),
     path('login',SignInView.as_view(),name='cust_signin'),
-    path('home',HomePageView.as_view(),name='customer_home')
+    path('home',HomePageView.as_view(),name='customer_home'),
+    path("update",update_details,name="update")
+
 
 ]
