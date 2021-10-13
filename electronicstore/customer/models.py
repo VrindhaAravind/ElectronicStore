067f85dce4d6bfa10afbb48644f5861511eaf5ff
sandeepks23 <81618525+sandeepks23@users.noreply.github.com>
2021-10-13 11:18:42 +0530

    Cart model added

--- electronicstore/customer/models.py
+++ electronicstore/customer/models.py
@@ -9,3 +9,11 @@ class Userdetails(models.Model):
 
     def __str__(self):
         return self.first_name
+    
+class Cart(models.Model):
+    product=models.ForeignKey(Products,on_delete=models.CASCADE)
+    user=models.CharField(max_length=120)
+    options=(("ordernotplaced","ordernotplaced"),
+             ("orderplaced","orderplaced")
+             )
+    status=models.CharField(max_length=120,choices=options,default="ordernotplaced")
