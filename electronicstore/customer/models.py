6b46265b1bd8629f556ac7c9a2159de4ffa4a777
Vrindha Aravind <86411764+VrindhaAravind@users.noreply.github.com>
2021-10-28 11:11:45 +0530

    Update models.py

--- electronicstore/customer/models.py
+++ electronicstore/customer/models.py
@@ -4,53 +4,54 @@ from django.contrib.auth.models import User
 
 
 class Userdetails(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     mobile_number = models.CharField(max_length=15, blank=False)
     dob = models.DateField(blank=True, null=True)
-    image = models.ImageField(default="default-avatar.png" ,upload_to="images",null=True, blank=True)
+    image = models.ImageField(default="default-avatar.png", upload_to="images", null=True, blank=True)
 
     def __str__(self):
-        return self.first_name
+        return self.user.username
 
 
 class Cart(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
-    user = models.CharField(max_length=120)
-    quantity=models.IntegerField(default=1)
+    user = models.CharField(max_length=130)
+    quantity = models.IntegerField(default=1)
     options = (("ordernotplaced", "ordernotplaced"),
                ("orderplaced", "orderplaced")
                )
-    status = models.CharField(max_length=120, choices=options, default="ordernotplaced")c
+    status = models.CharField(max_length=120, choices=options, default="ordernotplaced")
+
 
 class Orders(models.Model):
-    product=models.ForeignKey(Products,on_delete=models.CASCADE)
-    user=models.CharField(max_length=120)
-    address=models.TextField()
-    seller = models.CharField(max_length=250,default=None)
-    quantity = models.IntegerField(default=1)
-    options=(
-        ('pending','pending'),
-        ("ordered","ordered"),
-        ("packed","packed"),
-        ("shipped","shipped"),
-        ("delivered","delivered"),
-        ("cancelled","cancelled")
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    address = models.TextField()
+    seller = models.CharField(max_length=250, default=None)
+    options = (
+        ("pending","pending"),
+        ("ordered", "ordered"),
+        ("packed", "packed"),
+        ("shipped", "shipped"),
+        ("delivered", "delivered"),
+        ("cancelled", "cancelled")
     )
-    status=models.CharField(max_length=120,choices=options,default="pending")
-    date=models.DateField(auto_now=True)
-    
- class Address(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100)
-    mob_no=models.CharField(max_length=30)
-    house=models.CharField(max_length=150)
-    street=models.CharField(max_length=150)
-    town=models.CharField(max_length=100)
-    state=models.CharField(max_length=100)
-    pin=models.CharField(max_length=30)
-    landmark=models.CharField(max_length=150)
+    status = models.CharField(max_length=120, choices=options, default="ordered")
+    date = models.DateField(auto_now=True)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    mob_no = models.CharField(max_length=30)
+    house = models.CharField(max_length=150)
+    street = models.CharField(max_length=150)
+    town = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pin = models.CharField(max_length=30)
+    landmark = models.CharField(max_length=150)
 
 
 class Review(models.Model):
@@ -58,13 +59,14 @@ class Review(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     review = models.CharField(max_length=500)
     date = models.DateField(auto_now=True)
-    
+
+
 class CustomerService(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    subject=models.CharField(max_length=100,default=None)
-    message=models.CharField(max_length=500)
-    response=models.CharField(max_length=500,default=None,blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, default=None)
+    message = models.CharField(max_length=500)
+    response = models.CharField(max_length=500, default=None, blank=True, null=True)
     date = models.DateField(auto_now=True)
-    
+
     def __str__(self):
         return self.user.email
